d746884d544cf0469c8ed4e9f81a42b2f72f1a6a
Prabesh <iprabesh@hotmail.com>
2018-03-12 19:23:14 -0500

    taking that fail off

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -61,7 +61,6 @@ class CarsController < ApplicationController
   def update
     respond_to do |format|
       if @car.update(car_params)
-        fail
         format.html { redirect_to @car, notice: 'Car was successfully updated.' }
         format.json { render :show, status: :ok, location: @car }
       else
