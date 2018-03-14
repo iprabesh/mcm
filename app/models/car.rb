01971d9d997e6987c17a00ddc37bffb12b5234fe
Prabesh <iprabesh@hotmail.com>
2018-03-13 22:30:22 -0500

    Added cloudinary for paperclip gem

--- app/models/car.rb
+++ app/models/car.rb
@@ -5,7 +5,8 @@ class Car < ApplicationRecord
 	belongs_to :user
 
 	has_attached_file :imageclip, styles: { medium: "300x300>", thumb: "100x100>" }
-  	validates_attachment_content_type :imageclip, content_type: /\Aimage\/.*\z/
+  	validates_attachment_content_type :imageclip, content_type: /\Aimage\/.*\z/,
+  	:storage => :cloudinary
 
   	validates :brand, presence: true
 
